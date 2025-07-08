d6ae78584fa39f0a7ab67a70979625ae40a3487a
Varad <varadchaudari51@gmail.com>
2025-07-08 13:24:36 -0700

    Update project files

--- app.py
+++ app.py
@@ -1,35 +1,10 @@
-from flask import Flask, request, send_file, render_template_string
+from flask import Flask, request, send_file, render_template
 import yt_dlp
 import os
 import tempfile
 import mimetypes
 
 app = Flask(__name__)
-
-HTML = '''
-<!DOCTYPE html>
-<html>
-<head>
-  <title>YouTube Downloader</title>
-</head>
-<body>
-  <h2>YouTube Video Downloader</h2>
-  <form method="POST">
-    <input type="text" name="url" placeholder="Enter YouTube URL" required>
-    <br><br>
-    <label>Select Quality:</label>
-    <select name="quality">
-      <option value="360">360p</option>
-      <option value="480">480p</option>
-      <option value="720">720p</option>
-      <option value="1080">1080p</option>
-    </select>
-    <br><br>
-    <button type="submit">Download</button>
-  </form>
-</body>
-</html>
-'''
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -60,7 +35,7 @@ def index():
                     mimetype=mimetype or "application/octet-stream"
                 )
 
-    return render_template_string(HTML)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
